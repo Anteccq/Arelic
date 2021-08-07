64f1e0ac417910ed71955264b3be190888984124
AntiqueR <antique.pendulum@gmail.com>
2021-08-08 02:15:43 +0900

    Initial commit

---
+++ Arelic/Arelic/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using Arelic.Views;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Arelic
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+
+            CreateDefaultBuilder((config, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+            }).GetService<MainWindow>()?.Activate();
+        }
+
+        private static ServiceProvider CreateDefaultBuilder(Action<IConfiguration, IServiceCollection> configureService)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .Build();
+            IServiceCollection services = new ServiceCollection();
+            configureService?.Invoke(config, services);
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Arelic
+ A password manager for Windows (x86, x64). 
