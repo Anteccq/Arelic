5dd20bf1350dfa5af649d2cd5a55807283edcf3f
AntiqueR <antique.pendulum@gmail.com>
2021-08-08 22:21:44 +0900

    力技で解決した

---
+++ Arelic/Arelic/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+using Arelic.Models;
+//using Reactive.Bindings;
+
+namespace Arelic.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<Block> Blocks { get; } = new();
+
+        //public ReactiveProperty<bool> IsEditMode { get; } = new();
+    }
+}
