d4eca67314a3ac627c905fb67abec0c27d346877
AntiqueR <antique.pendulum@gmail.com>
2021-08-08 19:47:33 +0900

    add block service

---
+++ Arelic/Arelic/Models/Services/IBlockService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Arelic.Models.Services
+{
+    internal interface IBlockService
+    {
+        public Task<List<Block>> GetBlocksAsync(string password, string filePath, CancellationToken cancellationToken = default);
+
+        public Task SetBlocksAsync(string password, string filePath, List<Block> blocks, CancellationToken cancellationToken = default);
+    }
+}
